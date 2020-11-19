85ef382fa8413e05fc9e96fef8e67d3255862d1f
FFFFOX <2680211421@qq.com>
2020-11-19 15:05:49 +0800

    司机折叠卡片

--- src/views/current/driver.vue
+++ src/views/current/driver.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div>
     <el-card class="bgCard">
       <p>运输中司机：</p>
@@ -14,7 +15,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -25,7 +44,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -36,7 +73,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -49,7 +104,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -60,7 +133,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -71,7 +162,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -97,7 +206,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -108,7 +235,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -119,7 +264,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -132,7 +295,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -143,7 +324,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -154,7 +353,25 @@
                   
                 </div>
                 <div v-for="o in 1" :key="o" class="text item">
-                  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                  <el-collapse v-model="activeNames" @change="handleChange">
+                    <el-collapse-item title="一致性 Consistency" name="1">
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="反馈 Feedback" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="效率 Efficiency" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="可控 Controllability" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
                 </div>
               </el-card>
             </td>
@@ -220,32 +437,13 @@
   export default {
     data() {
       return {
-        data: [{
-          label: '信息1',
-          children: [{
-            label: '二级 1-1',
-          }]
-        }, {
-          label: '信息2',
-          children: [{
-            label: '二级 1-1',
-          }]
-        }, {
-          label: '信息3',
-          children: [{
-            label: '二级 1-1',
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
+        activeNames: ['1']
       };
     },
     methods: {
-      handleNodeClick(data) {
-        console.log(data);
+      handleChange(val) {
+        console.log(val);
       }
     }
-  };
+  }
 </script>
