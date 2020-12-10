9888b8c71acadc9cb9be406048947725e7f5f157
zhaofelix <15021179915@163.com>
2020-12-10 14:43:42 +0800

    feat:实时司机信息接入

--- src/api/driver/index.js
+++ src/api/driver/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2020-12-01 07:55:34
+ * @LastEditTime: 2020-12-10 13:46:41
+ * @FilePath: /pc-front/src/api/driver/index.js
+ * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+ */
 import request from '@/utils/request'
 import { base_path } from '@/config/config'
 
@@ -39,5 +47,12 @@ export function getDriverListByKeyword(params) {
         url: `${base_path}driver/query/queryByKeyword`,
         method: 'get',
         params
+    })
+}
+
+export function getonGoingDriver() {
+    return request({
+        url: `${base_path}ongoing/driver`,
+        method: 'get',
     })
 }
--- src/views/current/driver.vue
+++ src/views/current/driver.vue
@@ -1,8 +1,9 @@
 <!--
+import { import } from '@babel/types';
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-01 07:55:34
- * @LastEditTime: 2020-12-06 16:35:18
+ * @LastEditTime: 2020-12-10 14:42:56
  * @FilePath: /pc-front/src/views/current/driver.vue
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
 -->
@@ -11,190 +12,176 @@
     <el-card class="bgCard">
       <p>运输中司机：</p>
       <div>
-        <center>
-          <table class="cardTable">
-            <tr>
-              <td>
-                <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                    <span>司机</span>
-                  </div>
-                  <div v-for="o in 1" :key="o" class="text item">
-                    <el-collapse v-model="activeNames" @change="handleChange">
-                      <el-collapse-item title="订单号" name="1">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="具体状态" name="2">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="实时行程" name="3">
-                        <div>
-                          <img
-                            src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2214980092,2961462071&fm=26&gp=0.jpg"
-                          />
-                        </div>
-                      </el-collapse-item>
-                      <el-collapse-item title="联系方式" name="4">
-                        <div>content</div>
-                      </el-collapse-item>
-                    </el-collapse>
-                  </div>
-                </el-card>
-              </td>
-              <td>
-                <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                    <span>司机</span>
-                  </div>
-                  <div v-for="o in 1" :key="o" class="text item">
-                    <el-collapse v-model="activeNames" @change="handleChange">
-                      <el-collapse-item title="订单号" name="1">
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="具体状态" name="2">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="实时行程" name="3">
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="联系方式" name="4">
-                        <div>content</div>
-                      </el-collapse-item>
-                    </el-collapse>
-                  </div>
-                </el-card>
-              </td>
-              <td>
-                <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                    <span>司机</span>
-                  </div>
-                  <div v-for="o in 1" :key="o" class="text item">
-                    <el-collapse v-model="activeNames" @change="handleChange">
-                      <el-collapse-item title="订单号" name="1">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="具体状态" name="2">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="实时行程" name="3">
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="联系方式" name="4">
-                        <div>content</div>
-                      </el-collapse-item>
-                    </el-collapse>
-                  </div>
-                </el-card>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                    <span>司机</span>
-                  </div>
-                  <div v-for="o in 1" :key="o" class="text item">
-                    <el-collapse v-model="activeNames" @change="handleChange">
-                      <el-collapse-item title="订单号" name="1">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="具体状态" name="2">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="实时行程" name="3">
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="联系方式" name="4">
-                        <div>content</div>
-                      </el-collapse-item>
-                    </el-collapse>
-                  </div>
-                </el-card>
-              </td>
-              <td>
-                <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                    <span>司机</span>
-                  </div>
-                  <div v-for="o in 1" :key="o" class="text item">
-                    <el-collapse v-model="activeNames" @change="handleChange">
-                      <el-collapse-item title="订单号" name="1">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="具体状态" name="2">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="实时行程" name="3">
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="联系方式" name="4">
-                        <div>content</div>
-                      </el-collapse-item>
-                    </el-collapse>
-                  </div>
-                </el-card>
-              </td>
-              <td>
-                <el-card class="box-card">
-                  <div slot="header" class="clearfix">
-                    <span>司机</span>
-                  </div>
-                  <div v-for="o in 1" :key="o" class="text item">
-                    <el-collapse v-model="activeNames" @change="handleChange">
-                      <el-collapse-item title="订单号" name="1">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="具体状态" name="2">
-                        <div>content</div>
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="实时行程" name="3">
-                        <div>content</div>
-                      </el-collapse-item>
-                      <el-collapse-item title="联系方式" name="4">
-                        <div>content</div>
-                      </el-collapse-item>
-                    </el-collapse>
-                  </div>
-                </el-card>
-              </td>
-            </tr>
-          </table>
-        </center>
+        <el-card class="cardTable" v-for="(item, index) in list" :key="index">
+          <div slot="header" class="clearfix">
+            <span>{{ "订单号：" + item.order_number }}</span>
+            <span class="order-status">{{
+              item.order_status == 6 ? "已完成" : "进行中"
+            }}</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            <el-collapse v-model="activeNames" @change="handleChange">
+              <el-collapse-item title="用户信息" name="1">
+                <div>
+                  <span class="title-style">
+                    用户名：
+                  </span>
+                  <span>
+                    {{ item.order_user_name }}
+                  </span>
+                </div>
+                <div>
+                  <span class="title-style">
+                    订单地址：
+                  </span>
+                  <span>
+                    {{ item.user_address }}
+                  </span>
+                </div>
+                <div>
+                  <span class="title-style">渣土图片:</span>
+                  <center>
+                    <viewer :images="[]">
+                      <img
+                        v-for="(item, index) in JSON.parse(
+                          item.user_place_order_img
+                        )"
+                        :key="index"
+                        :src="item.url"
+                        alt=""
+                        class="image-thumb"
+                      />
+                    </viewer>
+                  </center>
+                </div>
+              </el-collapse-item>
+              <el-collapse-item title="司机信息" name="2">
+                <div>
+                  <span class="title-style">
+                    司机姓名：
+                  </span>
+                  <span>
+                    {{ item.driver_name }}
+                  </span>
+                </div>
+                <div>
+                  <span class="title-style">
+                    车牌号：
+                  </span>
+                  <span>
+                    {{ item.car_number }}
+                  </span>
+                </div>
+                <div>
+                  <span class="title-style">
+                    司机电话：
+                  </span>
+                  <span>
+                    {{ item.driver_phone }}
+                  </span>
+                </div>
+              </el-collapse-item>
+              <el-collapse-item title="实时行程" name="3">
+                <div v-if="item.driver_reach_img != null">
+                  <span class="title-style">渣土现场:</span>
+                  <center>
+                    <viewer :images="[]">
+                      <img
+                        v-for="(item, index) in JSON.parse(
+                          item.user_place_order_img
+                        )"
+                        :key="index"
+                        :src="item.url"
+                        alt=""
+                        class="image-thumb"
+                      />
+                    </viewer>
+                  </center>
+                  <span class="title-style">处理时间：</span>
+                  <span>2020-12-23 14:00:00</span>
+                </div>
+                <div v-if="item.driver_reach_img != null" class="divider"></div>
+                <div v-if="item.driver_get_img != null">
+                  <span class="title-style">渣土装车:</span>
+                  <center>
+                    <viewer :images="[]">
+                      <img
+                        v-for="(item, index) in JSON.parse(
+                          item.user_place_order_img
+                        )"
+                        :key="index"
+                        :src="item.url"
+                        alt=""
+                        class="image-thumb"
+                      />
+                    </viewer>
+                  </center>
+                  <span class="title-style">处理时间：</span>
+                  <span>2020-12-23 14:00:00</span>
+                </div>
+                <div class="divider"></div>
+                <div v-if="item.driver_complete_img != null">
+                  <span class="title-style">渣土倾倒:</span>
+                  <center>
+                    <viewer :images="[]">
+                      <img
+                        v-for="(item, index) in JSON.parse(
+                          item.user_place_order_img
+                        )"
+                        :key="index"
+                        :src="item.url"
+                        alt=""
+                        class="image-thumb"
+                      />
+                    </viewer>
+                  </center>
+                  <span class="title-style">处理时间：</span>
+                  <span>2020-12-23 14:00:00</span>
+                </div>
+              </el-collapse-item>
+              <!-- <el-collapse-item title="联系方式" name="4">
+                <div>content</div>
+              </el-collapse-item> -->
+            </el-collapse>
+          </div>
+        </el-card>
       </div>
       <br />
     </el-card>
   </div>
 </template>
+<script>
+import { getonGoingDriver } from "@/api/driver";
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+  methods: {
+    fetchData() {
+      getonGoingDriver().then(response => {
+        this.list = response.data;
+        this.listLoading = false;
+      });
+    },
+    handleChange(val) {
+      console.log(val);
+    }
+  },
+  created() {
+    this.fetchData();
+  }
+};
+</script>
 
 <style scoped>
-.onMission {
-  background: rgb(226, 76, 76);
-}
-.free {
-  background: rgb(63, 188, 197);
-}
 .cardTable {
-  width: 97%;
+  width: 25%;
   margin: 1%;
 }
 .text {
   font-size: 14px;
-}
-
-.item {
-  margin-bottom: 18px;
 }
 
 .clearfix:before,
@@ -207,57 +194,29 @@
 }
 
 .bgCard {
-  margin: 3%;
-  width: 95%;
+  margin: 1%;
+  width: 98%;
 }
 
-.box-card {
-  width: 400px;
-  /* margin: 5%; */
+.image-thumb {
+  width: 25%;
+  height: 80px;
+  padding: 5px;
+}
 
-  margin-bottom: 2%;
-  /* margin-left: 10%; */
-  /* margin-right: 10%; */
-}</style
->>
+.title-style {
+  font-weight: bold;
+}
 
-<script>
-export default {
-  methods: {
-    handleChange(val) {
-      console.log(val);
-    }
-  }
-};
-</script>
+.divider {
+  height: 1px;
+  background: rgb(0, 0, 0, 0.05);
+  width: 100%;
+  margin: 8px 0;
+}
 
-<script>
-export default {
-  data() {
-    return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
-    };
-  }
-};
-</script>
+.order-status {
+  float: right;
+  color: red;
+}
+</style>
