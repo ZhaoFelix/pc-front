1483cbfa38cbb9def63f8ce744050315e8ac0261
zhaofelix <15021179915@163.com>
2021-01-07 09:25:51 +0800

    fix:改用原生SDK加载地图

--- src/views/current/map2.vue
+++ src/views/current/map2.vue
@@ -3,7 +3,7 @@ import { import } from '@babel/types';
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2021-01-07 08:21:38
- * @LastEditTime: 2021-01-07 09:08:26
+ * @LastEditTime: 2021-01-07 09:22:07
  * @FilePath: /pc-front/src/views/current/map2.vue
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
 -->
@@ -12,9 +12,8 @@ import { import } from '@babel/types';
     <el-amap
       class="amap-box"
       ref="map"
-      :vid="'amap-vue'"
+      vid="amap-vue"
       :amap-manager="amapManger"
-      :center="center"
       :zoom="zoom"
       :plugin="plugin"
       :events="events"
@@ -24,26 +23,19 @@ import { import } from '@babel/types';
 </template>
 
 <script>
-import { AMapManager } from "vue-amap";
+import { AMapManager, lazyAMapApiLoaderInstance } from "vue-amap";
 
 export default {
   data() {
     return {
+      map: null,
       zoom: 16,
-      center: [121.59996, 31.197646],
+
       events: {
-        init: o => {
-          console.log(o.getCenter());
-          console.log(this.$refs.map.$$getInstance());
-          o.getCity(result => {
-            console.log(result);
-          });
-        },
+        init: o => {},
         moveend: () => {},
         zoomchange: () => {},
-        click: e => {
-          alert("map clicked");
-        }
+        click: e => {}
       },
       plugin: [
         "ToolBar",
@@ -58,6 +50,15 @@ export default {
         }
       ]
     };
+  },
+  mounted() {
+    lazyAMapApiLoaderInstance.load().then(() => {
+      // your code ...
+      this.map = new AMap.Map("amap-vue", {
+        center: new AMap.LngLat(121.59996, 31.197646),
+        zoom: this.zoom
+      });
+    });
   }
 };
 </script>
