7538a588f7489f92d27109d080a30711df9c96c9
zhaofelix <15021179915@163.com>
2021-05-25 09:04:33 +0800

    fix:根据时间段查询销量

--- src/views/analysis/index.vue
+++ src/views/analysis/index.vue
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2021-05-24 13:35:16
- * @LastEditTime: 2021-05-24 15:01:55
+ * @LastEditTime: 2021-05-25 08:54:56
  * @FilePath: /pc-front/src/views/analysis/index.vue
  * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
 -->
@@ -12,6 +12,27 @@
     <el-row :gutter="16">
       <el-col :span="24">
         <div class="chart-wrapper">
+          <el-row>
+            <el-col :offset="9" :span="6">
+              <el-date-picker
+                v-model="timeGap"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                value-format="yyyy-MM-dd"
+              >
+              </el-date-picker>
+              <span style="padding:8px"> </span>
+              <el-button
+                size="medium"
+                @click="queryTimeGap"
+                :type="isSearch ? 'danger' : 'success'"
+                >{{ !isSearch ? "查询" : "取消" }}</el-button
+              >
+            </el-col>
+          </el-row>
+          <div style="padding:8px"></div>
           <bar-chart
             title="最近30天销售"
             :chart-data="saleLineChartData"
@@ -26,7 +47,7 @@
 import PanelGroup from "./components/PanelGroup";
 import BarChart from "./components/BarChart";
 import { querySale } from "@/api/dashboard";
-
+import { queryAnalysisTimeSale } from "@/api/analysis";
 const saleLineChartData = {
   countData: [],
   totalData: []
@@ -44,25 +65,16 @@ export default {
   data() {
     return {
       saleLineChartData,
-      saleXData
+      saleXData,
+      timeGap: "",
+      isSearch: false
     };
   },
   methods: {
     fetchData() {
       querySale({ timeGap: 30 }).then(response => {
         let result = response.data;
-        let days = [];
-        let count = [];
-        let total = [];
-        for (var i = 0; i < result.length; i++) {
-          let item = result[i];
-          days.push(item.days);
-          count.push(item.count);
-          total.push(item.total);
-        }
-        this.saleXData = days;
-        this.saleLineChartData.countData = count;
-        this.saleLineChartData.totalData = total;
+        this.generateBarData(result);
       });
     },
     handleSetLineChartData() {
@@ -80,6 +92,47 @@ export default {
         this.XData = days;
         this.lineChartData.expectedData = count;
       });
+    },
+    generateBarData(result) {
+      // 生成新数据前先置空
+      this.saleLineChartData.countData = [];
+      this.saleLineChartData.totalData = [];
+      let days = [];
+      let count = [];
+      let total = [];
+      for (var i = 0; i < result.length; i++) {
+        let item = result[i];
+        days.push(item.days);
+        count.push(item.count);
+        total.push(item.total);
+      }
+      this.saleXData = days;
+      this.saleLineChartData.countData = count;
+      this.saleLineChartData.totalData = total;
+    },
+    queryTimeGap() {
+      if (this.isSearch) {
+        this.isSearch = !this.isSearch;
+        this.timeGap = "";
+        this.fetchData();
+        return;
+      }
+      if (this.timeGap != "") {
+        queryAnalysisTimeSale({
+          startDate: this.timeGap[0],
+          endDate: this.timeGap[1]
+        }).then(response => {
+          let result = response.data;
+          if (result.length == 0) {
+            this.$message("查询数据为空，请重新选择查询时间段！");
+            return;
+          }
+          this.generateBarData(result);
+          this.isSearch = true;
+        });
+      } else {
+        this.$message("请先选择查询时间段！");
+      }
     }
   },
   mounted() {
